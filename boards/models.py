5d6c25ff2a4528665b5ed3270fbc083d0d2c3390
LENOVO <enocksimiyu8@gmail.com>
2023-02-08 17:59:48 +0300

    Part 3 - Forms and creating new topic

--- boards/models.py
+++ boards/models.py
@@ -27,3 +27,4 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
+
